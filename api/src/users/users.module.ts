67dbc104332d3d31a501dd986a94adc11a2f9074
Mateus Sousa <sousa.programador@gmail.com>
2024-06-11 21:31:56 -0300

    part one, test integrations

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from '@/auth/auth.service';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  providers: [UsersService],
+  providers: [UsersService, JwtService, AuthService],
   controllers: [UsersController],
   exports: [UsersService],
 })
